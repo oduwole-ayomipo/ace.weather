cc76fe2ebcf265a0602dc1a883962ed95293ccf0
Oduwole Ayomipo <harkorede318@gmail.com>
2023-10-01 02:01:41 +0100

    Project Environment Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import * as Unicons from '@iconscout/react-unicons';
+
+function App() {
+  return (
+    <div className="App">
+     <p className='text-red-500'>Hello World!</p>
+     <Unicons.UilReact/>
+    </div>
+  );
+}
+
+export default App;
